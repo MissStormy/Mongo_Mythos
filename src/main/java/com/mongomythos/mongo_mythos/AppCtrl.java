bfa2b00e246723650fc3992afbe209e51a029819
Lara RG <laragoblin@outlook.com>
2023-11-02 14:13:57 +0100

    FrontEnd finished

---
+++ src/main/java/com/mongomythos/mongo_mythos/AppCtrl.java
@@ -0,0 +1,98 @@
+package com.mongomythos.mongo_mythos;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+public class AppCtrl {
+    @FXML
+    private Button CleanBtn;
+
+    @FXML
+    private ComboBox<?> GeneroCmb;
+
+    @FXML
+    private TableColumn<?, ?> GeneroFullTbc;
+
+    @FXML
+    private Button ListBtn;
+
+    @FXML
+    private AnchorPane ListPane;
+
+    @FXML
+    private Button ModifyBtn;
+
+    @FXML
+    private TableView<?> MythoTbv;
+
+    @FXML
+    private TableView<?> MythosFullTbv;
+
+    @FXML
+    private Button NewBtn;
+
+    @FXML
+    private Button NewDataBtn;
+
+    @FXML
+    private TableColumn<?, ?> NombreFullTbc;
+
+    @FXML
+    private TableColumn<?, ?> NombreTbc;
+
+    @FXML
+    private TextField NombreTxt;
+
+    @FXML
+    private TableColumn<?, ?> OrigenFullTbc;
+
+    @FXML
+    private TextField OrigenTxt;
+
+    @FXML
+    private Button SaveBtn;
+
+    @FXML
+    private AnchorPane SaveDataPane;
+
+    @FXML
+    private ComboBox<?> TipoCmb;
+
+    @FXML
+    private TableColumn<?, ?> TipoFullTbc;
+
+    @FXML
+    private TableColumn<?, ?> TipoTbc;
+
+    @FXML
+    void OnClickClean(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickList(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickModify(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickNew(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickNewData(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickSave(ActionEvent event) {
+
+    }
+}
